f41951364e1db56285f07fc5da7238168777c433
SheeRMooN <sheermood@gmail.com>
2021-05-01 22:38:18 +0300

    Initial commit

---
+++ src/test/java/com/example/controller/StandaloneControllerTests.java
@@ -0,0 +1,47 @@
+package com.example.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.example.model.Employee;
+import com.example.service.EmployeeService;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(controllers = EmployeeController.class)
+public class StandaloneControllerTests {
+ 
+    @MockBean
+    EmployeeService employeeService;
+ 
+    @Autowired
+    MockMvc mockMvc;
+ 
+    @Test
+    public void testfindAll() throws Exception {
+        Employee employee = new Employee("Lokesh", "Gupta");
+        List<Employee> employees = Arrays.asList(employee);
+ 
+        Mockito.when(employeeService.findAll()).thenReturn(employees);
+ 
+        mockMvc.perform(get("/employee"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", Matchers.hasSize(1)))
+                .andExpect(jsonPath("$[0].firstName", Matchers.is("Lokesh")));
+    }
+
+}
